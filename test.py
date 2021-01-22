9533e15ac0a2630b924281dd219f1258f49e5748
Bermmie-Laptop <cbchun@lsis.com>
2021-01-22 22:47:53 +0900

    비행기 움직이는 것 까지 완성

--- test.py
+++ test.py
@@ -4,6 +4,7 @@ pygame.init()
 
 # CONSTANTS
 WHITE = (255, 255, 255)
+RED = (255, 0, 0)
 done = False
 clock = pygame.time.Clock()
 
@@ -11,6 +12,8 @@ RIGHT = pygame.K_RIGHT
 LEFT = pygame.K_LEFT
 UP = pygame.K_UP
 DOWN = pygame.K_DOWN
+SPACEBAR = pygame.K_SPACE
+
 
 class Board:
     def __init__(self):
@@ -28,13 +31,31 @@ class Plane:
         figure = pygame.transform.scale(figure, (60, 45))
         return figure
 
-    def move(self, key):
-        if key == 
+    def move(self, event):
+        if event.key == DOWN:
+            self.y += 10
+        elif event.key == UP:
+            self.y -= 10
+        else:
+            pass
+
 
 class Missile:
     def __init__(self, plane_x, plane_y):
         self.x = plane_x + 30
         self.y = plane_y + 30
+
+    def draw(self, screen, event):
+        figure = pygame.Rect(self.x + 30, self.y - 10, 3, 3)
+        figure = pygame.draw.rect(screen, RED, figure)
+
+        if event.key == SPACEBAR:
+            return figure
+        else:
+            pass
+
+    def move(self, event, screen):
+        pass
 
 
 screen = pygame.display.set_mode(Board().size)
@@ -43,18 +64,28 @@ screen = pygame.display.set_mode(Board().size)
 def runGame():
     board = Board()
     plane = Plane()
-    missile = Missile(plane.x, plane.y)
 
     global done
     while not done:
+        missile = Missile(plane.x, plane.y)
+
         clock.tick(10)
         screen.fill(board.color)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 done = True
+            else:
+                pass
+
+            if event.type == pygame.KEYDOWN:
+                plane.move(event)
+                missile.draw(screen, event)
+            else:
+                pass
 
         screen.blit(plane.draw(), (plane.x, plane.y))
+
         pygame.display.update()
 
 
